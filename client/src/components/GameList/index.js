f0cd299e00b867ec604b56b6f6ec8c94f3307518
Anthony Pena <apena5@alumni.jh.edu>
2021-09-29 02:28:06 -0400

    fixed console log errors

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const GameList = () => {
 
     return (
 
-    <div class="dropdown">
-        <button class="dropbtn btn d-block w-30">Choose a Game</button>
-        <div class="dropdown-content">
-            <a href="/minecraft">Minecraft</a>
-            <a href="/fortnite">Fortnite</a>
-            <a href="/leagueoflegends">League Of Legends</a>
-            <a href="/pubg">PUBG</a>
-            <a href="/apexlegends">Apex Legends</a>
+        <div class="dropdown">
+            <button className="dropbtn btn d-block w-30">Choose a Game</button>
+            <div className="dropdown-content">
+                <a href="/minecraft">Minecraft</a>
+                <a href="/fortnite">Fortnite</a>
+                <a href="/leagueoflegends">League Of Legends</a>
+                <a href="/pubg">PUBG</a>
+                <a href="/apexlegends">Apex Legends</a>
+            </div>
         </div>
-    </div>
     );
 };
 
