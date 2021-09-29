eb861a14932f7e60b6ec9070e71f6adc3fbc23b9
McHenry <lukemchenry311@gmail.com>
2021-09-29 02:10:20 -0600

    fixed final errors with capitalization

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -12,11 +12,11 @@ const GameList = () => {
     <div class="dropdown">
         <button class="special-button">Choose a Game</button>
         <div class="dropdown-content">
-            <a class="minecraft" href="/minecraft" alt="minecraft"></a>
-            <a class="fortnite" href="/fortnite" alt="fortnite"></a>
-            <a class="league" href="/leagueoflegends" alt="league of legends"></a>
-            <a class="pubg" href="/pubg" alt="player unknown battlegrounds"></a>
-            <a class="apex" href="/apexlegends" alt="apex legends"></a>
+            <a class="minecraft" href="/minecraft" alt="minecraft">minecraft</a>
+            <a class="fortnite" href="/fortnite" alt="fortnite">fortnite</a>
+            <a class="league" href="/leagueoflegends" alt="league of legends">league</a>
+            <a class="pubg" href="/pubg" alt="player unknown battlegrounds">pubg</a>
+            <a class="apex" href="/apexlegends" alt="apex legends">apex legends</a>
         </div>
     </div>
     );
