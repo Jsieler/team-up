566fda822ab72f6ea67c06f7064eabee94cbdf16
McHenry <lukemchenry311@gmail.com>
2021-09-30 16:30:29 -0600

    mouseover/woes

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -6,7 +6,7 @@ const GameList = () => {
 
         <div className="dropdown">
             <button className="special-button">Choose a Game</button>
-            <div className="dropdown-content">
+            <div className="dropdown-content">          
                 <a className="minecraft" href="/game/Minecraft" alt="minecraft">minecraft</a>
                 <a className="fortnite" href="/fortnite" alt="fortnite">fortnite</a>
                 <a className="league" href="/leagueoflegends" alt="league of legends">league</a>
