eb0ac39f67e7539ddeb1dc5a080afd7579f5165c
Anthony Pena <apena5@alumni.jh.edu>
2021-09-29 20:09:23 -0400

    menu fix

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -20,7 +20,7 @@ const GameList = () => {
         <div className="dropdown">
             <button className="special-button">Choose a Game</button>
             <div className="dropdown-content">
-                <a className="minecraft" href="/minecraft" alt="minecraft">minecraft</a>
+                <a className="minecraft" href="/game/Minecraft" alt="minecraft">minecraft</a>
                 <a className="fortnite" href="/fortnite" alt="fortnite">fortnite</a>
                 <a className="league" href="/leagueoflegends" alt="league of legends">league</a>
                 <a className="pubg" href="/pubg" alt="player unknown battlegrounds">pubg</a>
